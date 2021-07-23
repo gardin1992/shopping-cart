416f31d1afd61cfe9dce390fd24cad91db39afff
Joao Gardin <gardin1992@gmail.com>
2021-07-23 12:13:41 -0300

    fix detalhes

--- src/screens/orders/OrderDetailsScreen.tsx
+++ src/screens/orders/OrderDetailsScreen.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import useIndexedStore from '../../hooks/useIndexedStore';
 import { toDecimal } from '../../mask';
 
@@ -41,29 +41,33 @@ function OrderDetailsScreen() {
 
     return <div className="fluid">
 
+        <Link to="/pedidos">
+            <a href="/pedidos">Voltar</a>
+        </Link>
+
         <h2>Detalhes do pedido Ref.: {id}</h2>
 
-        <p><strong>SubTotal</strong>: ${toDecimal(order.subTotal)}</p>
-        <p><strong>Desconto</strong>: ${toDecimal(order.discount)}</p>
-        <p><strong>Total Pago</strong>: ${toDecimal(order.total)}</p>
+        <p><strong>SubTotal</strong>: ${toDecimal(order?.subTotal)}</p>
+        <p><strong>Desconto</strong>: ${toDecimal(order?.discount)}</p>
+        <p><strong>Total Pago</strong>: ${toDecimal(order?.total)}</p>
 
         <h2>Detalhes do cliente</h2>
 
-        <p><strong>Nome</strong>: {order.user?.name}</p>
-        <p><strong>CPF</strong>: {order.user?.name}</p>
+        <p><strong>Nome</strong>: {order?.user?.name}</p>
+        <p><strong>CPF</strong>: {order?.user?.cpf}</p>
 
         <p>Endereço</p>
-        <p><strong>CEP</strong>: {order.user?.postalCode}</p>
-        <p><strong>Logradouro</strong>: {order.user?.streetName}</p>
-        <p><strong>Número</strong>: {order.user?.streetNumber}</p>
-        <p><strong>Bairro</strong>: {order.user?.neighborhood}</p>
-        <p><strong>Cidade</strong>: {order.user?.city}</p>
-        <p><strong>UF</strong>: {order.user?.state}</p>
+        <p><strong>CEP</strong>: {order?.user?.postalCode}</p>
+        <p><strong>Logradouro</strong>: {order?.user?.streetName}</p>
+        <p><strong>Número</strong>: {order?.user?.streetNumber}</p>
+        <p><strong>Bairro</strong>: {order?.user?.neighborhood}</p>
+        <p><strong>Cidade</strong>: {order?.user?.city}</p>
+        <p><strong>UF</strong>: {order?.user?.state}</p>
 
-        <h2>Items Comprados ({order.amount} items)</h2>
+        <h2>Items Comprados ({order?.amount} items)</h2>
 
         <table>
-            {order.products.map((product: any) => <tr>
+            {order?.products.map((product: any) => <tr>
                 <td>{product.title}</td>
                 <td>{product.amount}</td>
                 <td>${toDecimal(product.price)}</td>
